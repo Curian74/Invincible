5d10c65e859694d00c73275e450711a5f3025b01
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-03-15 11:08:01 +0700

    Add borders to map

--- Assets/Scripts/Demo/PoolingTest.cs
+++ Assets/Scripts/Demo/PoolingTest.cs
@@ -1,46 +1,46 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class PoolingTest : MonoBehaviour
-{
-    [SerializeField] private float spawnRate = 1;
-    public Player playerPrefab;
-    public PoolingManager<Player> playerPool;
-    private float spawnRateTimer = 0;
-    void Start()
-    {
-        playerPool = new PoolingManager<Player>(playerPrefab, 5, transform);
-    }
+// public class PoolingTest : MonoBehaviour
+// {
+//     [SerializeField] private float spawnRate = 1;
+//     public Player playerPrefab;
+//     public PoolingManager<Player> playerPool;
+//     private float spawnRateTimer = 0;
+//     void Start()
+//     {
+//         playerPool = new PoolingManager<Player>(playerPrefab, 5, transform);
+//     }
 
-    void Update()
-    {
-        spawnRateTimer += Time.deltaTime;
-        if(spawnRateTimer >= spawnRate)
-        {
-            SpawnPlayer();
-            spawnRateTimer = 0;
-        }
-    }
+//     void Update()
+//     {
+//         spawnRateTimer += Time.deltaTime;
+//         if(spawnRateTimer >= spawnRate)
+//         {
+//             SpawnPlayer();
+//             spawnRateTimer = 0;
+//         }
+//     }
 
-    public Player SpawnPlayer()
-    {
-        Player newPlayer = playerPool.GetObject();
+//     public Player SpawnPlayer()
+//     {
+//         Player newPlayer = playerPool.GetObject();
 
-        if (newPlayer == null)
-        {
-            return null;
-        }
+//         if (newPlayer == null)
+//         {
+//             return null;
+//         }
 
-        Vector2 randomPosition = new Vector2(
-            Random.Range(1, 4),
-            Random.Range(1, 3)
-        );
+//         Vector2 randomPosition = new Vector2(
+//             Random.Range(1, 4),
+//             Random.Range(1, 3)
+//         );
 
-        newPlayer.transform.position = randomPosition;
-        return newPlayer;
-    }
+//         newPlayer.transform.position = randomPosition;
+//         return newPlayer;
+//     }
 
-    public void ReturnPlayer(Player player)
-    {
-        playerPool.BackToPool(player);
-    }
-}
+//     public void ReturnPlayer(Player player)
+//     {
+//         playerPool.BackToPool(player);
+//     }
+// }
