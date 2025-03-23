d36b429c581714a808997a47d2b00a2e5d302c99
QuocAnh1502 <anhcnqhe187233@fpt.edu.vn>
2025-03-24 01:26:13 +0700

    attack skeleton, movement & some projectile

--- Assets/Scripts/Boss/Mecha Golem/MechaGolemAttacks.cs
+++ Assets/Scripts/Boss/Mecha Golem/MechaGolemAttacks.cs
@@ -1,16 +1,105 @@
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MechaGolemAttacks : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Rock _rockPrefab;
+    [SerializeField] private int _rockPool = 5;
+    [SerializeField] Transform _armFirePoint;
+    private Transform _player;
+    private Animator _animator;
+    private PoolingManager<Rock> _poolingManager;
+    private bool _isPerformingAction = false;
+    void Awake()
     {
+        _player = GameObject.FindWithTag("Player").transform;
+        _animator = GetComponent<Animator>();
+        _poolingManager = new PoolingManager<Rock>(_rockPrefab, _rockPool, null);
+    }
+
+    
+    void Update()
+    {
+        if (_player != null && _player.gameObject.activeInHierarchy)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, _player.position);
+            if (!_isPerformingAction)
+            {
+                StartCoroutine(PerformRandomAttack(GetRandomDecision()));
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator PerformRandomAttack(int decision)
     {
-        
+        _isPerformingAction = true;
+        switch (decision)
+        {
+            //case 1:
+            //    yield return StartCoroutine(ThrowRock());
+            //    break;
+            //case 2:
+            //    yield return StartCoroutine(Defend());
+            //    break;
+            //case 3:
+            //    yield return StartCoroutine(ShootLaser());
+            //    break;
+
+            case 1:
+                yield return StartCoroutine(Defend());
+                break;
+            case 2:
+                yield return StartCoroutine(Defend());
+                break;
+            case 3:
+                yield return StartCoroutine(Defend());
+                break;
+        }
+         yield return new WaitForSeconds(3f);
+        _isPerformingAction = false;
+    }
+
+    private IEnumerator ThrowRock()
+    {
+        var rock = _poolingManager.GetObject();
+        if (rock != null)
+        {
+            if(_player.transform.position.x > transform.position.x)
+            {
+                transform.localScale = new Vector3(6, 6, 1);
+            }
+            else
+            {
+                {
+                    transform.localScale = new Vector3(-6, 6, 1);
+                }
+            }
+            _animator.SetTrigger("Throw");
+            rock.transform.position = _armFirePoint.position;
+            rock.gameObject.SetActive(true);
+            yield return new WaitForSeconds(1f);
+            _animator.SetTrigger("Idle");     
+        }
+    }
+
+    private IEnumerator Defend()
+    {
+        _animator.SetTrigger("Defend");
+        yield return new WaitForSeconds(2f);
+        _animator.SetTrigger("Idle");
+    }
+
+    private IEnumerator ShootLaser()
+    {
+        _animator.SetTrigger("Laser");
+        yield return new WaitForSeconds(1.5f);
+        _animator.SetTrigger("Idle");
+    }
+
+    private int GetRandomDecision()
+    {
+        return Random.Range(1, 4);
     }
 }
--- Assets/Scripts/Boss/Mecha Golem/MechaGolemMovements.cs
+++ Assets/Scripts/Boss/Mecha Golem/MechaGolemMovements.cs
@@ -2,37 +2,54 @@
 
 public class MechaGolemMovements : MonoBehaviour
 {
-    [SerializeField] private float moveRadius = 4f;
-    [SerializeField] private float moveSpeed = 10f; // Thêm tốc độ di chuyển
+    [SerializeField] private float moveRadius = 7f;
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float minDistance = 3f;
     private Rigidbody2D _rb;
     private Animator _animator;
     private Transform _player;
     private Vector2 targetPosition;
+    private SpriteRenderer _spriteRenderer;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _player = GameObject.FindWithTag("Player").transform;
+        _spriteRenderer = GetComponent<SpriteRenderer>(); // Get sprite renderer
     }
 
     void Start()
     {
-        InvokeRepeating(nameof(SetNewTargetPosition), 0f, 3f); // Gọi mỗi 3 giây để đổi mục tiêu
+        InvokeRepeating(nameof(SetNewTargetPosition), 0f, 3f);
     }
 
     void Update()
     {
+        float distanceToPlayer = Vector3.Distance(transform.position, _player.position);
+        if(IsPlayingAnimation("Laser") || IsPlayingAnimation("Defend"))
+        {
+            return;
+        }
+        if (distanceToPlayer <= minDistance)
+        {
+            MoveAway();
+        }
         MoveToTarget();
     }
 
     private void SetNewTargetPosition()
     {
         if (_player == null) return;
-
-        // Chọn một vị trí ngẫu nhiên xung quanh người chơi
         Vector2 randomDirection = Random.insideUnitCircle.normalized * moveRadius;
         targetPosition = (Vector2)_player.position + randomDirection;
+    }
+
+    private void MoveAway()
+    {
+        Vector2 directionAway = (transform.position - _player.position).normalized;
+        targetPosition = (Vector2)transform.position + directionAway * moveRadius;
+        MoveToTarget();
     }
 
     private void MoveToTarget()
@@ -40,6 +57,21 @@ public class MechaGolemMovements : MonoBehaviour
         if ((Vector2)transform.position != targetPosition)
         {
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            FlipSprite(targetPosition);
         }
+    }
+
+    public void FlipSprite(Vector2 target)
+    {
+        if (target.x > transform.position.x)
+            transform.localScale = new Vector3(6, 6, 1);  
+        else if (target.x < transform.position.x)
+            transform.localScale = new Vector3(-6, 6, 1); 
+    }
+
+    private bool IsPlayingAnimation(string animationName)
+    {
+        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        return stateInfo.IsName(animationName) && stateInfo.normalizedTime < 1.0f;
     }
 }
