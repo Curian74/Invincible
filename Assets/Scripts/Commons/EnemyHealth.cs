5d5a898e5b3cab94c1eeee7d7f468715176098e8
Odysseus <136612227+TheOldScratchZ@users.noreply.github.com>
2025-03-24 21:44:51 +0700

    Update sound explode khi enemy ban muoi

--- Assets/Scripts/Commons/EnemyHealth.cs
+++ Assets/Scripts/Commons/EnemyHealth.cs
@@ -5,32 +5,43 @@ public class EnemyHealth : Health
     [SerializeField] private GameObject[] powerupPrefabs;
     [SerializeField] private float dropChance = 0.5f;
     [SerializeField] private float healthBonus = 15f;
-    
+    [SerializeField] private AudioClip _deathSound;
+
     private float baseMaxHealth;
 
     private new void Awake()
     {
-         base.Awake();
+        base.Awake();
         baseMaxHealth = maxHealth;
     }
-    
+
     private void OnEnable()
     {
         maxHealth = baseMaxHealth;
-        
+
         int currentCycle = Spawner.GetCurrentCycle();
         if (currentCycle > 0)
         {
             maxHealth += healthBonus * currentCycle;
             Debug.Log($"Applied health bonus of {healthBonus * currentCycle} to enemy. New maxHealth: {maxHealth}");
         }
-        
+
         currentHealth = maxHealth;
         UpdateHealthUI();
     }
 
     protected override void Die()
     {
+        if (_deathSound != null)
+        {
+            AudioSource.PlayClipAtPoint(_deathSound, transform.position, 1f); // Đảm bảo âm lượng là 1f
+            Debug.Log("Death sound played.");
+        }
+        else
+        {
+            Debug.LogWarning("No death sound assigned to this enemy.");
+        }
+
         TryDropPowerup();
         base.Die();
     }
